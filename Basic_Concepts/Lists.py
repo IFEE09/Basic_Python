029e3aa9492051f5a26f04108d8185e73c58bece
IFEE09 <76457505+IFEE09@users.noreply.github.com>
2021-01-07 11:16:52 -0600

    fixing something
    thanks

--- Basic_Concepts/Lists.py
+++ Basic_Concepts/Lists.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5, 6] #of numbers 
-objects = ["Hello", 2, 4.5, True, "World"] #Every object
+objects = ["Hello", 2, 4.5, True, "World"] #aAny object
 
 #we can access like a string or array of strings
 print(objects[0]) #access to element 1, position 0
